79f646594fb9a2ffb155703ec1a62a0cd7435a83
Martin H Berwanger <martin.berwanger@gmail.com>
2013-09-16 12:25:26 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+It490::Application.config.secret_key_base = '206726efbf5421a5b162b55fa2c6525d8bda60f6c932b3791db585c2883100ee357e2b59a0f557999b66c4450a8ae3ddb2c59cbdfa40de9e5c769826d99133fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+It490::Application.config.session_store :cookie_store, key: '_it490_session'
