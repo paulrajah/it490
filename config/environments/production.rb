70cb619eb55b29af67c21e634ba2ffb2b57c2129
Martin H Berwanger <martin.berwanger@gmail.com>
2013-10-10 17:11:48 -0400

    added postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ It490::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  config.action_mailer.default_url_options = { :host => 'it490.herokuapp.com' }
 end
